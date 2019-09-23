a7bfabb52f1d17b79f9cf2ef41457ec65ec1b7bf
Vigneshwaran Ganesan <vigneshwaran@Vigneshwarans-MacBook-Pro.local>
2019-09-22 22:34:29 -0500

    Polymorphism using Abstract method

--- PolymorphismUsingAbstract.py
+++ PolymorphismUsingAbstract.py
@@ -2,5 +2,25 @@ class Animal:
     def __init__(self, name):
         self.name = name
 
+    # ABSTRACT METHOD
     def speak(self):
         raise NotImplementedError("Subclass must will implement this abstract method")
+
+
+class Dog(Animal):
+    # ABSTRACT METHOD IS OVERRIDDEN HERE
+    def speak(self):
+        return self.name + " says woof"
+
+
+class Cat(Animal):
+    # ABSTRACT METHOD IS OVERRIDDEN HERE
+    def speak(self):
+        return self.name + " says meow"
+
+
+myDog = Dog("Jimmy")
+print(myDog.speak())
+
+myCat = Cat("Tom")
+print(myCat.speak())
